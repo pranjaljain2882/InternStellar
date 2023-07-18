ae79d29230822c3588310ade992130ae29bd62a2
pranjaljain2882 <jain.pranjal12345@gmail.com>
2023-07-18 21:53:34 +0530

    Final

--- src/components/Pages/Home/Sliders/dataSlider.js
+++ src/components/Pages/Home/Sliders/dataSlider.js
@@ -1,6 +1,6 @@
 import img1 from '../../../images/slider1.png'
-// import img2 from '../../../images/slider2.svg'
-// import img3 from '../../../images/slider3.svg'
+import img2 from '../../../images/slider2.png'
+import img3 from '../../../images/slider3.png'
 
 const dataSlider = [
     {
@@ -9,12 +9,12 @@ const dataSlider = [
       title: "Internships may not require physical strength, but initiative definitely does"
     },
     {
-      img: img1,
+      img: img2,
       id: 2,
       title: "Grow through what you go through!!"
     },
     {
-      img: img1,
+      img: img3,
       id: 3,
       title: "Your one-step solution for your jobs and intership related queries"
     },
